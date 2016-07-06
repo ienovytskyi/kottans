4556db617599a6381ed50203b8d93e3c7688a8c5
Ievgen Novytskyi <kjubi2008@gmail.com>
2016-07-06 13:36:57 +0300

    spec symbols patch

--- week06/kata1/index.js
+++ week06/kata1/index.js
@@ -1,16 +1,21 @@
+/*
+delimiter may has [] and may not. multiple delimiters use with [] only for each delim
+delimiter may contain spec symbl, BUT ALWAYS shield "\"
+*/
+
 "use strict"
 
 const index = strings =>
 {
 	if (strings == "") return 0
 	
-	let array = strings.match(/^\/\/(?:\[?(.+?)\]?)\n/)				//delimiter may has [] and may not. multiple delimiters use with [] only
+	let array = strings.match(/^\/\/(?:\[*(.+?)\]*)\n/)				
 
 	if (array)
 	{
 		let [{length}, delim] = array
 		strings = strings.slice(length)
-		var pattern = new RegExp(delim.split('][').join("|"), "g")
+		var pattern = new RegExp(delim.split('][').map( item => item.replace(/[\(\)\[\]\.\^\$\|\?\+\*\=\!\\\{\}\s]/g, "\\$&")).join("|"), "g")		
 	}
 	else pattern = /[,\n]/g
 
@@ -21,8 +26,8 @@ const index = strings =>
 //		.filter(string => string < 1001)							//looks good but we parse array one more time
 		.map( (string, index, array) => string > 1000 ? false : 	//looks bad but we parse array only once
 			string < 0 ? 		
-				temp.push(string) && index == array.length-1 ? eval("throw new Error ('Negative numbers ['+ temp +'] not allowed')") : false
-			: temp.length!=0 && index == array.length-1 ? eval("throw new Error ('Negative numbers ['+ temp +'] not allowed')") 
+				temp.push(string) && index == array.length-1 ? eval("throw new Error ('Negative numbers ['+ temp +'] not allowed')") : false	//if negative is the last
+			: temp.length!=0 && index == array.length-1 ? eval("throw new Error ('Negative numbers ['+ temp +'] not allowed')") 				//if positive is the last but we also have negatives
 				: string == "" ? NaN : +string )
 		.reduce((sum, num) => sum + num)
 
